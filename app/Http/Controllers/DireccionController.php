9f63fe01195c39df010a50e23a2a70d1e19b3f01
Desarrollo Web <60014100+Desarrollo-ewmx@users.noreply.github.com>
2022-05-24 13:17:44 -0500

    Regresar Direcciones

--- app/Http/Controllers/DireccionController.php
+++ app/Http/Controllers/DireccionController.php
@@ -15,19 +15,8 @@ class DireccionController extends Controller
     public function index()
     {
         //
-        // $direccion = Direccion::get(); 
-        // return $direccion;
-        // return \Request::ip();
-    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
-        if (array_key_exists($key, $_SERVER) === true){
-            foreach (explode(',', $_SERVER[$key]) as $ip){
-                $ip = trim($ip); // just to be safe
-                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-                    return $ip;
-                }
-            }
-        }
-    }
+        $direccion = Direccion::get(); 
+        return $direccion;
     }
 
     /**
